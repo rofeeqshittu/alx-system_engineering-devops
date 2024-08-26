e2a7add32590620d6c010d584de8ac53f19faed8
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-08-26 22:31:11 +0100

    re 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python3
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
-def fetch_data(url):
-    """Fetch data from the API."""
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Failed to fetch data.")
-        return []
-
-def organize_data(tasks):
-    """Organize tasks data by user ID."""
-    organized_data = {}
-    for task in tasks:
-        user_id = str(task['userId'])
-        if user_id not in organized_data:
-            organized_data[user_id] = []
-        task_info = {
-            "username": task['username'],
-            "task": task['title'],
-            "completed": task['completed']
-        }
-        organized_data[user_id].append(task_info)
-    return organized_data
-
-def export_to_json(data, filename):
-    """Export data to a JSON file."""
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
-
-def main():
-    url = "https://jsonplaceholder.typicode.com/todos"
-    tasks = fetch_data(url)
-    if tasks:
-        organized_data = organize_data(tasks)
-        export_to_json(organized_data, "todo_all_employees.json")
-
 if __name__ == "__main__":
-    main()
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
