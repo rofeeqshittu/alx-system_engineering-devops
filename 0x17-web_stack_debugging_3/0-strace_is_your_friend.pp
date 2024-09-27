4625c2569ba745c554c75fe89d744950f90c28f1
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-09-27 11:15:21 +0100

    fixes puppetlint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,29 @@
-# 0-strace_is_your_friend.pp
-# Puppet manifest to fix WordPress directory permissions
+# This Puppet manifest fixes issues causing Apache to return a 500 error
 
-file { '/var/www/html/your_wordpress_directory':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-  recurse => true,
+# Fix file permissions for the WordPress directory
+file { '/var/www/html/wp-content':
+  ensure => directory,
+  mode   => '0755',
 }
 
-file { '/var/www/html/your_wordpress_directory/*':
-  ensure => file,
+# Ensure Apache is running
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
+
+# Ensure the WordPress directory is owned by the right user
+file { '/var/www/html':
   owner  => 'www-data',
   group  => 'www-data',
-  mode   => '0644',
+  mode   => '0755',
+  require => Service['apache2'],
+}
+
+# Additional configuration or fix as identified from strace
+# If a specific file was missing or needed, you can include that as well
+exec { 'fix-wordpress':
+  command => '/usr/bin/wget -P /var/www/html/ https://example.com/path/to/missing/file.php',
+  onlyif  => '/usr/bin/test ! -f /var/www/html/file.php',
 }
 
