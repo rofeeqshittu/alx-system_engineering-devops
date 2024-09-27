1e47c90d8bed7051c0de82569991881f8e074862
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-09-27 11:09:52 +0100

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,18 @@
+# 0-strace_is_your_friend.pp
+# Puppet manifest to fix WordPress directory permissions
+
+file { '/var/www/html/your_wordpress_directory':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
+  recurse => true,
+}
+
+file { '/var/www/html/your_wordpress_directory/*':
+  ensure => file,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0644',
+}
+
