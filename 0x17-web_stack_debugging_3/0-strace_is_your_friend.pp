fe32ea46a9b8c1080889a4303b10336c01ef2b39
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-09-27 11:35:55 +0100

    groundup redo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,27 +1,6 @@
-# This Puppet manifest fixes issues causing Apache to return a 500 error
+# automated puppet fix (to find out why Apache is returning a 500 error)
 
-# Fix the WordPress site by correcting the file extension in wp-settings.php
 exec { 'Fix wordpress site':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => 'shell',
-  onlyif   => 'test -f /var/www/html/wp-settings.php && grep -q ".phpp" /var/www/html/wp-settings.php',
+  provider => shell,
 }
-
-# Ensure Apache is running after the fix
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
-# Optional: Notify to restart Apache after the fix
-notify { 'Restart Apache':
-  refreshonly => true,
-}
-
-# Ensure Apache restarts if the fix command executes successfully
-exec { 'Restart Apache':
-  command     => '/bin/systemctl restart apache2',
-  subscribe   => Exec['Fix wordpress site'],
-  refreshonly => true,
-}
-
